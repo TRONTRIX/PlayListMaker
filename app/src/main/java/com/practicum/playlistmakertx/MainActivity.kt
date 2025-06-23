9584521dc6559cf1d10cc6572e5676b50a20958c
TRONTRIX <TheDrof3cool@mail.ru>
2025-06-23 10:26:30 +0300

    Initial commit

---
+++ app/src/main/java/com/practicum/playlistmakertx/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.practicum.playlistmakertx
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val buttonSearch = findViewById<Button>(R.id.search_button)
+        val buttonSearchClickListener: View.OnClickListener = object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на Поиск", Toast.LENGTH_SHORT).show()
+            }
+        }
+        buttonSearch.setOnClickListener(buttonSearchClickListener)
+
+        val buttonLibrary = findViewById<Button>(R.id.library_button)
+        buttonLibrary.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на Библиотеку", Toast.LENGTH_SHORT).show()
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
