e28d71b30bef142057bf1bf565dc2e092b7614ba
TRONTRIX <TheDrof3cool@mail.ru>
2025-06-23 13:57:08 +0300

    2 коммит, убраны тосты, добавлена навигация

--- app/src/main/java/com/practicum/playlistmakertx/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmakertx/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.practicum.playlistmakertx
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -16,18 +17,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val buttonSearch = findViewById<Button>(R.id.search_button)
-        val buttonSearchClickListener: View.OnClickListener = object : View.OnClickListener{
-            override fun onClick(v: View?) {
-                Toast.makeText(this@MainActivity, "Нажали на Поиск!", Toast.LENGTH_SHORT).show()
-            }
+        buttonSearch.setOnClickListener{
+            val searchIntent = Intent(this, SearchActivity::class.java)
+            startActivity(searchIntent)
         }
-        buttonSearch.setOnClickListener(buttonSearchClickListener)
 
         val buttonLibrary = findViewById<Button>(R.id.library_button)
         buttonLibrary.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Нажали на Библиотеку!",Toast.LENGTH_SHORT).show()
+            val libraryIntent = Intent(this, LibraryActivity::class.java)
+            startActivity(libraryIntent)
         }
 
+        val buttonSetting = findViewById<Button>(R.id.setting_button)
+
+        buttonSetting.setOnClickListener {
+            val settingIntent = Intent(this, SettingActivity::class.java)
+            startActivity(settingIntent)
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
